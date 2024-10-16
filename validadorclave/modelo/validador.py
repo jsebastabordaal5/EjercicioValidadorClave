3af646dbded06fbbd9af25f244bd22706ee5f4df
Juanse <jtaborda579@soyudemedellin.edu.co>
2024-10-15 21:32:43 -0500

    Create classes and functions

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,34 @@
-# TODO: Implementa el código del ejercicio aquí
+class ReglaValidacion:
+
+    def __init__(self):
+        self._longitud_esperada = _longitud_esperada
+
+    def _validar_longitud(self):
+        pass
+
+    def _contiene_mayuscula(self):
+        pass
+
+    def _contiene_minuscula(self):
+        pass
+
+    def _contiene_numero(self):
+        pass
+
+
+class ReglaValidacionGanimedes:
+
+    def contiene_caracter_especial(self):
+        pass
+
+    def es_valida(self):
+        pass
+
+
+class ReglaValidacionCalisto:
+
+    def contiene_calisto(self):
+        pass
+
+    def es_valida(self):
+        pass
